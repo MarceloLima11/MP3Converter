f4c738390206faa72e81bb8eecd86eb9f8922f3e
MarceloLima11 <marcelolima152021@gmail.com>
2024-02-21 11:27:35 -0300

    wip: create video handler

---
+++ Server/Api/Controllers/ConverterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConverterController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ConverterVideo(string videoLink)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception err) 
+            { return BadRequest(err.Message); }
+        }
+    }
+}
---
+++ Server/App/Handler/VideoHandler.cs
@@ -0,0 +1,17 @@
+﻿using YoutubeExplode;
+
+namespace App.Handler
+{
+    public class VideoHandler
+    {
+        public VideoHandler()
+        {}
+
+        public async void GetVideo(string videoLink)
+        {
+            var youtube = new YoutubeClient();
+
+            var video = await youtube.Videos.GetAsync(videoLink);
+        }
+    }
+}
