35752888f2932ae3be1c97922774f97202333694
MarceloLima11 <marcelolima152021@gmail.com>
2024-03-14 02:44:07 -0300

    wip: mid container convertor

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -6,7 +6,12 @@ function Home() {
         <main>
             <Header />
             <div className='converter_container'>
-                a
+                <h1>Faster <span>YouTube</span> to MP3 converter</h1>
+
+                <p>Get and download YouTube videos for free</p>
+
+                <input type='text' placeholder='https://www.youtube.com/watch?v=aTMtiakjpks' required />
+                <button></button>
             </div>
         </main>
     );
